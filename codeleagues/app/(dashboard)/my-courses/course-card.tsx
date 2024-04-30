ff397edb77dad1b653781bc1cac23f5288f694df
VetitK <gmvetit20@gmail.com>
2024-05-01 00:25:37 +0700

    dynamic course card

--- codeleagues/app/(dashboard)/my-courses/course-card.tsx
+++ codeleagues/app/(dashboard)/my-courses/course-card.tsx
@@ -1,33 +1,34 @@
 "use client";
+import { CourseCardProps } from "@/types/types";
 
-export function CourseCard() {
-
+export function CourseCard(props: CourseCardProps) {
     return (
         <article className="card border-solid border-2 px-10 py-6 gap-3">
-            <img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes"className="rounded-lg"/>
-                <div className="card-actions justify-start">
-                    <p className="badge badge-outline px-3 py-2 h-fit bg-gray-50">Python</p>
-                    <p className="badge badge-outline px-3 py-2 h-fit">ผู้เริ่มต้น</p>
-                    <p className="badge badge-outline px-3 py-2 h-fit">คลาสสด</p>
-                </div>
+            <img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" className="rounded-lg" />
+            <div className="card-actions justify-start">
+                <p className="badge badge-outline px-3 py-2 h-fit bg-gray-50">{props.course || "Python"}</p>
+                <p className="badge badge-outline px-3 py-2 h-fit">{props.difficulty || "Beginner"}</p>
+                <p className="badge badge-outline px-3 py-2 h-fit">{props.class_type || "Live"}</p>
+            </div>
 
-                <div className="avatar-group -space-x-2 rtl:space-x-reverse">
-                    <div className="avatar border-0">
-                        <div className="w-12">
-                            <img src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
-                        </div>
-                    </div>
-                    <div className="avatar border-0">
-                        <div className="w-12">
-                            <img src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
-                        </div>
-                    </div>
-                </div>
+            <div className="avatar-group -space-x-2 rtl:space-x-reverse">
+                {
+                    props.avatar_url.map((avatar, index) => {
+                        return (
+                            <div className="avatar border-0" key={index}>
+                                <div className="w-12">
+                                    <img src={avatar} />
+                                </div>
+                            </div>
+                        );
+                    })
+                }
+            </div>
 
-                <h3 className="card-title">
-                Python เลเวล 2
-                </h3>
-                <p>คลาสสด Python ระดับเริ่มต้นไม่มีพื้นฐานก็เรียนได้</p>
+            <h3 className="card-title">
+                {props.title || "?"}
+            </h3>
+            <p>{props.description || "?"}</p>
         </article>
     );
 }
--- codeleagues/app/(dashboard)/my-courses/page.tsx
+++ codeleagues/app/(dashboard)/my-courses/page.tsx
@@ -2,28 +2,69 @@ import Link from "next/link";
 import { headers } from "next/headers";
 import { createClient } from "@/utils/supabase/server";
 import { redirect } from "next/navigation";
-import { SubmitButton } from "./submit-button";
 import Image from "next/image";
 import locLogo from "@/app/static/LOCLogoNoBG.png";
 import loginImage from "@/app/static/classroom.jpeg"
 import { CourseCard } from "./course-card"
+import { CourseCardProps } from "@/types/types";
 
 export default function MyCourses() {
+
+  // Query the database for the courses that the user is currently enrolled in
+
+  const cards_info: CourseCardProps[] = [
+    {
+      title: "Python 2",
+      description: "Learn the intermediate of programming with Python",
+      image: "https://images.unsplash.com/photo-1622838321164-5c3f5f1e4c7d",
+      course: "Python",
+      difficulty: "Intermediate",
+      class_type: "Live",
+      avatar_url: ["https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"]
+    },
+    {
+      title: "Python 3",
+      description: "Learn the advance of programming with Python",
+      image: "https://images.unsplash.com/photo-1622838321164-5c3f5f1e4c7d",
+      course: "Python",
+      difficulty: "Advance",
+      class_type: "Live",
+      avatar_url: ["https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"]
+    },
+    
+  ]
+
+  const completed_cards_info: CourseCardProps[] = [
+    {
+      title: "Python 1",
+      description: "Learn the basics of programming with Python",
+      image: "https://images.unsplash.com/photo-1622838321164-5c3f5f1e4c7d",
+      course: "Python Level 1",
+      difficulty: "Beginner",
+      class_type: "Live",
+      avatar_url: ["https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"]
+    },
+    
+  ]
 
   return (
     <>
       <h1 className="text-3xl">Courses</h1>
       <section aria-labelledby="current-courses" className="grid grid-cols-3 gap-6 my-4">
         <h2 id="current-courses" className="col-span-3 text-2xl">Current Courses</h2>
-        <CourseCard />
-        <CourseCard />
-        <CourseCard />
-        <CourseCard />
+        {
+          cards_info.map((card, index) => {
+            return <CourseCard key={index} {...card} />
+          })
+        }
       </section>
       <section aria-labelledby="completed-courses" className="grid grid-cols-3 gap-6 my-4">
         <h2 id="completed-courses" className="col-span-3 text-2xl">Completed Courses</h2>
-        <CourseCard />
-        <CourseCard />
+        {
+          completed_cards_info.map((card, index) => {
+            return <CourseCard key={index} {...card} />
+          })
+        }
       </section>
     </>
   );
