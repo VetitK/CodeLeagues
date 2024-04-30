5f4f252b4c14f3095c18bd86feb95e33c37f10f2
VetitK <gmvetit20@gmail.com>
2024-05-01 00:25:12 +0700

    add type file

---
+++ codeleagues/types/types.d.ts
@@ -0,0 +1,9 @@
+export type CourseCardProps = {
+    title: string;
+    description: string;
+    image: string;
+    course: string;
+    difficulty: string;
+    class_type: string;
+    avatar_url: string[];
+};
